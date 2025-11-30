8400f26d55dc02e18d9c68c9b38b1e9b07d76282
Brrrtmrt <69765218+Brrrtmrt@users.noreply.github.com>
2025-11-30 18:52:13 +0300

    changed structure and added methods

---
+++ src/main/java/Core/Graph.java
@@ -0,0 +1,11 @@
+package Core;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
+
+public class Graph {
+    public static HashMap<String, HashSet<String>> createGraph(final ArrayList<String> courses) {
+        return null;
+    }
+}
