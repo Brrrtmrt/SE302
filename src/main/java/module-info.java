e5c96643617ac2660e7f63ee8285943bd5f4ec16
EfeAtasoy <efeatasoy808@gmail.com>
2025-12-25 14:55:50 +0300

    sum
    added localedata module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.se {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires jdk.localedata;
 
     exports GUI;
     opens GUI to javafx.fxml;
